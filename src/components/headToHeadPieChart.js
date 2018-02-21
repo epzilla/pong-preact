cc7295e92f85571b21fa9e554c854130317523c2
Adam Epling <adamepling@gmail.com>
2018-02-21 16:36:15 -0600

    Pies for matches, games, and points.

--- src/components/headToHeadPieChart.js
+++ src/components/headToHeadPieChart.js
@@ -1,39 +1,44 @@
 import { PieChart, Pie, ResponsiveContainer, Sector, Cell } from 'recharts';
 
-const COLORS = ['#0088FE', '#FFBB28', '#00C49F', '#FF8042'];
+const COLORS = ['#0088FE', '#00C49F', '#00C49F'];
 const RADIAN = Math.PI / 180;
 
 const renderLabel = (props) => {
   const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
     fill, payload, label, percent, value, index } = props;
-    console.log(props);
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+  //   console.log(props);
+  // const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  // const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+  // const y = cy  + radius * Math.sin(-midAngle * RADIAN);
   if (label === 'Draws') {
     return;
   }
 
+  let labelFontSize = label.length > 10 ? (label.length > 15 ? 12 : 16) : 20;
+  let numberFontSize = value > 999 ? 14 : 20;
+  let numberOffset = value > 999 ? 22 : 18;
+
   if (index === 0) {
     return (
       <g>
-        <text x={cx - 150} y={cy} fill={fill} textAnchor={'middle'} font-size={"2rem"} dominantBaseline="central">
+        <text x={cx - 90} y={cy - 75} fill={fill} textAnchor={'middle'} font-size={`${labelFontSize}px`} dominantBaseline="central">
           {label}
         </text>
-        <text x={cx - 150} y={cy} dy={40} fill={fill} textAnchor={'middle'} font-size={"2rem"} dominantBaseline="central">
-          {Math.round(percent * 100)}%
+        <text x={cx - numberOffset} y={cy} dy={8} fill={fill} textAnchor={'middle'} font-size={`${numberFontSize}px`} dominantBaseline="central">
+          {value}
         </text>
+        <line x1={cx} x2={cx} y1={cy - 20} y2={cy + 20} stroke="#DEDEDE" stroke-width="1"/>
       </g>
     );
   }
 
   return (
     <g>
-      <text x={cx + 150} y={cy} fill={fill} textAnchor={'middle'} font-size={"2rem"} dominantBaseline="central">
+      <text x={cx + 90} y={cy + 75} fill={fill} textAnchor={'middle'} font-size={`${labelFontSize}px`}dominantBaseline="central">
         {label}
       </text>
-      <text x={cx + 150} y={cy} dy={40} fill={fill} textAnchor={'middle'} font-size={"2rem"} dominantBaseline="central">
-        {Math.round(percent * 100)}%
+      <text x={cx + numberOffset} y={cy} dy={8} fill={fill} textAnchor={'middle'} font-size={`${numberFontSize}px`}dominantBaseline="central">
+        {value}
       </text>
     </g>
   );
@@ -42,20 +47,20 @@ const renderLabel = (props) => {
 const renderActiveShape = (props) => {
   const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
     fill, payload, percent, value } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? 'start' : 'end';
+  // const sin = Math.sin(-RADIAN * midAngle);
+  // const cos = Math.cos(-RADIAN * midAngle);
+  // const sx = cx + (outerRadius + 10) * cos;
+  // const sy = cy + (outerRadius + 10) * sin;
+  // const mx = cx + (outerRadius + 30) * cos;
+  // const my = cy + (outerRadius + 30) * sin;
+  // const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  // const ey = my;
+  // const textAnchor = cos >= 0 ? 'start' : 'end';
 
   return (
     <g>
       <text x={cx} y={cy - 10} dy={0} textAnchor="middle" fill={"#777"}>{payload.label}</text>
-      <text x={cx} y={cy + 12} dy={10} textAnchor="middle" font-size={"2rem"} fill={"#777"}>{payload.wins}</text>
+      <text x={cx} y={cy + 12} dy={10} textAnchor="middle" font-size={"2rem"} fill={"#777"}>{value}</text>
       <Sector
         cx={cx}
         cy={cy}
@@ -69,20 +74,20 @@ const renderActiveShape = (props) => {
   );
 };
 
-const HeadToHeadPieChart = ({ pieData, activeIndex, onPieClick }) => {
+const HeadToHeadPieChart = ({ pieData, activeIndex, onPieClick, largestValue, longestLabel }) => {
+  const innerRadius = largestValue && largestValue > 999 ? 50 : 40;
   return (
-    <ResponsiveContainer width={"100%"} height={300}>
+    <ResponsiveContainer width={"100%"} height={200}>
       <PieChart>
         <Pie
           activeIndex={activeIndex}
-          activeShape={renderActiveShape}
           data={pieData}
           dataKey="wins"
           nameKey="label"
           cx="50%"
           cy="50%"
-          outerRadius={100}
-          innerRadius={50}
+          outerRadius={75}
+          innerRadius={innerRadius}
           fill="#8884d8"
           onClick={(data, index) => onPieClick(data, index)}
           label={renderLabel}
--- src/routes/stats.js
+++ src/routes/stats.js
@@ -7,41 +7,105 @@ export default class Stats extends Component {
     super(props);
     this.state = {
       stats: null,
-      pieData: null,
-      activeIndex: -1
+      matchesData: null,
+      gamesData: null,
+      pointsData: null,
+      activeMatchesIndex: -1,
+      activeGamesIndex: -1,
+      activePointsIndex: -1,
+      largestMatchesValue: 0,
+      largestGamesValue: 0,
+      largestPointsValue: 0
     };
   }
 
   componentDidMount() {
     Rest.get('stats/head-to-head/1/2').then(stats => {
-      let pieData = [
+      let matchesData = [
         { label: stats.player1.player.fname, wins: stats.player1.matchesWon },
-        { label: 'Draws', wins: stats.player1.matchesDrawn },
         { label: stats.player2.player.fname, wins: stats.player2.matchesWon }
       ];
-      this.setState({ stats, pieData });
+      if (stats.player1.matchesDrawn > 0) {
+        matchesData.splice(1, 0, { label: 'Draws', wins: stats.player1.matchesDrawn });
+      }
+      let gamesData = [
+        { label: stats.player1.player.fname, wins: stats.player1.gamesWon },
+        { label: stats.player2.player.fname, wins: stats.player2.gamesWon }
+      ];
+      let pointsData = [
+        { label: stats.player1.player.fname, wins: stats.player1.pointsFor },
+        { label: stats.player2.player.fname, wins: stats.player2.pointsFor }
+      ];
+      let largestMatchesValue = Math.max(stats.player1.matchesWon, stats.player2.matchesWon);
+      let largestGamesValue = Math.max(stats.player1.gamesWon, stats.player2.gamesWon);
+      let largestPointsValue = Math.max(stats.player1.pointsFor, stats.player2.pointsFor);
+      this.setState({ stats, matchesData, gamesData, pointsData, largestMatchesValue, largestGamesValue, largestPointsValue });
     });
   }
 
-  onPieClick = (data, index) => {
+  onMatchesPieClick = (data, index) => {
     this.setState({
-      activeIndex: index
+      activeMatchesIndex: index
+    });
+  };
+
+  onGamesPieClick = (data, index) => {
+    this.setState({
+      activeGamesIndex: index
+    });
+  };
+
+  onPointsPieClick = (data, index) => {
+    this.setState({
+      activePointsIndex: index
     });
   };
 
   render() {
-    return (
-      <div class="main stats">
-        <div class="pie-container">
-          {  this.state.stats &&
-            <HeadToHeadPieChart
-              onPieClick={this.onPieClick}
-              pieData={this.state.pieData}
-              activeIndex={this.state.activeIndex}
-            />
-          }
+    if (this.state.stats) {
+      return (
+        <div class="main stats">
+          <div class="charts-container">
+            <div class="chart-container">
+              <h3 class="chart-header">Matches Won</h3>
+              <div class="pie-container">
+                <HeadToHeadPieChart
+                  onPieClick={this.onMatchesPieClick}
+                  pieData={this.state.matchesData}
+                  activeIndex={this.state.activeMatchesIndex}
+                  largestValue={this.state.largestMatchesValue}
+                />
+                {
+                this.state.matchesData.length > 2 &&
+                <p class="draws-footnote">{this.state.stats.player1.matchesDrawn} Draws</p>
+              }
+              </div>
+            </div>
+            <div class="chart-container">
+              <h3 class="chart-header">Games Won</h3>
+              <div class="pie-container">
+                <HeadToHeadPieChart
+                  onPieClick={this.onGamesPieClick}
+                  pieData={this.state.gamesData}
+                  activeIndex={this.state.activeGamesIndex}
+                  largestValue={this.state.largestGamesValue}
+                />
+              </div>
+            </div>
+            <div class="chart-container">
+              <h3 class="chart-header">Points Won</h3>
+              <div class="pie-container">
+                <HeadToHeadPieChart
+                  onPieClick={this.onPointsPieClick}
+                  pieData={this.state.pointsData}
+                  activeIndex={this.state.activePointsIndex}
+                  largestValue={this.state.largestPointsValue}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
